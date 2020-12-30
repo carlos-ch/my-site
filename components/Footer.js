2d190adec8aa15a5bf92fa20c63ddd591e8e19df
carlos-ch <carlos.tschavez@gmail.com>
2020-12-30 13:23:57 +0100

    postcss and other configs

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,24 @@
 const links = [
-  { href: 'https://github.com/carlos-ch', label: 'GitHub' },
-  { href: 'https://www.linkedin.com/in/carlos-c-g/', label: 'LinkedIn' },
-  { href: 'https://twitter.com/carloselchavez', label: 'Twitter' },
+  { href: 'https://github.com/carlos-ch', label: 'GitHub', style: 'bg-Github' },
+  {
+    href: 'https://www.linkedin.com/in/carlos-c-g/',
+    label: 'LinkedIn',
+    style: 'bg-LinkedIn',
+  },
+  {
+    href: 'https://twitter.com/carloselchavez',
+    label: 'Twitter',
+    style: 'bg-Twitter',
+  },
 ];
 
 export default function Footer() {
   return (
     <div className="absolute w-full text-gray-text bg-gray-200 px-8 py-6 bottom-0">
       <dl className="flex justify-center  sm:float-right  mt-8 mb-12">
-        {links.map(({ href, label }) => (
+        {links.map(({ href, label, style }) => (
           <dt className="inline-block mx-spaced sm:mr-8">
-            <a
-              className={`block w-8 h-7 bg-social bg-${label}`}
-              href={href}
-            ></a>
+            <a className={`block w-8 h-7 bg-social ${style}`} href={href}></a>
           </dt>
         ))}
       </dl>
